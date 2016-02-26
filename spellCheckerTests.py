52cfdea569d6de3fddc44e27736312c45f477484
PSyn <zxb300@gmail.com>
2016-02-26 14:00:31 -0500

    Added files via upload

--- spellCheckerTests.py
+++ spellCheckerTests.py
@@ -1,4 +1,4 @@
-#Philip Kyd
+#Philip Kyd,Kevin Muriuki
 
 import unittest
 from spellChecker import *
@@ -9,24 +9,7 @@ class test_spellChecker(unittest.TestCase):
         self.test_dictionary = open("test_dictionary.txt", "r")
         self.test_dictionary_2 = open("test_dictionary_2.txt", "r")
 
-    def test_create_file_list(self):
-        dictionary_words = create_file_list(self.test_file)
-        #checks to make sure that the 0th element is the correct word
-        self.assertEqual("Do", dictionary_words[0] )
-        #checks to make sure that the 17th element is the correct word
-        self.assertEqual("shouldn't", dictionary_words[17] )
-        #checks to make sure the overall length of the list is exactly the number of words in the list
-        self.assertEqual(36, len(dictionary_words))
-
-    """
-    WHY DOES THE FOLLOWING NOT WORK?
     
-    def test_create_file_list(self):
-        self.assertEqual("Do", create_file_list(self.test_file)[0][0] )
-        self.assertEqual("shouldn't", create_file_list(self.test_file)[1][4] )
-
-    """
-
     def test_ignoreCaseAndPunc(self):
         #tests capitalization
         self.assertEqual("help", ignoreCaseAndPunc("Help"))
@@ -49,31 +32,31 @@ class test_spellChecker(unittest.TestCase):
     
     def test_findWordInDictionary(self):
         #checks to make sure a known word is recorded as in the dictionary
-        self.assertEqual(True, findWordInDictionary("you", self.test_dictionary))
+        self.assertTrue(findWordInDictionary("you","test_dictionary.txt"))
         #checks an additional word and capitalization and open/close status
-        self.assertEqual(True, findWordInDictionary("Animals", "test_dictionary.txt"))
+        self.assertTrue(findWordInDictionary("Animals", "test_dictionary.txt"))
         #checks to make sure punctuation has been removed
-        self.assertEqual(True, findWordInDictionary("animals,!", "test_dictionary.txt"))
+        self.assertTrue(findWordInDictionary("animals,!", "test_dictionary.txt"))
         #checks to make sure that correct values are returned when something isn't in the dictionary
-        self.assertEqual(False, findWordInDictionary("rabid", "test_dictionary.txt"))
+        self.assertFalse(findWordInDictionary("rabid", "test_dictionary.txt"))
         #checks to make sure that the return value is accurate
         self.assertFalse(findWordInDictionary("table", "test_dictionary.txt"))
 
     def test_getWordsOfSimLength(self):
         #checks to make sure that the list produced is accurate
-        self.assertEqual(["You", "Think", "Exist", "Are", "They", "Merry"], getWordsOfSimLength("done", "test_dictionary.txt", 1))
+        self.assertEqual(["you", "think", "exist", "are", "they", "merry"], getWordsOfSimLength("done", "test_dictionary.txt", 1))
         #checks a word that has equal length to a word in the dictionary 
-        self.assertEqual(["Think", "Animals", "Exist", "They", "Merry", "Affliction"], getWordsOfSimLength("Affable", "test_dictionary.txt", 3))
+        self.assertEqual(["think", "animals", "exist", "they", "merry", "affliction"], getWordsOfSimLength("Affable", "test_dictionary.txt", 3))
         #checks another extreme at the low end
-        self.assertEqual(["Do", "Or", "A",], getWordsOfSimLength("I", "test_dictionary.txt", 1))
+        self.assertEqual(["do", "or", "a",], getWordsOfSimLength("I", "test_dictionary.txt", 1))
         #checks a wide range tomake sure all words are included
-        self.assertEqual(["Animals", "Affliction"], getWordsOfSimLength("Happiness", "test_dictionary.txt", 3))
+        self.assertEqual(["animals", "affliction"], getWordsOfSimLength("Happiness", "test_dictionary.txt", 3))
         #checks another word
-        self.assertEqual(["Think", "Animals", "Exist", "They", "Merry", "Affliction"], getWordsOfSimLength("Affable", "test_dictionary.txt", 3))
+        self.assertEqual(["think", "animals", "exist", "they", "merry", "affliction"], getWordsOfSimLength("Affable", "test_dictionary.txt", 3))
         #checks to make sure that an empty list is returned when a word that has no matches is entered
         self.assertEqual([], getWordsOfSimLength("AppleBanananaCantelope", "test_dictionary.txt", 1))
         #checks an extreme range
-        self.assertEqual(["Do", "You", "Think", "Animals", "Exist", "Or", "Are", "They", "A", "Merry", "Affliction"], getWordsOfSimLength("Happiness", "test_dictionary.txt", 15))
+        self.assertEqual(["do", "you", "think", "animals", "exist", "or", "are", "they", "a", "merry", "affliction"], getWordsOfSimLength("Happiness", "test_dictionary.txt", 15))
 
     def test_getWordsWithSameStart(self):
         #matches first 2 characters
@@ -83,7 +66,15 @@ class test_spellChecker(unittest.TestCase):
         #makes sure that an empty list is returned if nothing matches
         #this also tests to make sure that the last character matching doesn't indicate a match
         self.assertEqual([], getWordsWithSameStart("Apples", ["Do", "You", "Think", "Animals", "Exist", "Or", "Are", "They", "A", "Merry", "Affliction"], 4))
-
+    def test_getWordsWithSameEnd(self):
+    
+        self.assertEqual(getWordsWithSameEnd('annimals',['animals','cannibas'],3),['animals'])
+        #test for output if now words match
+        self.assertEqual(getWordsWithSameEnd('ann',['pick','tick'],2),[])
+        #If outputs excceed number of common letters
+        self.assertEqual(getWordsWithSameEnd('aa',['aa','pick','fear'],3),['aa'])
+        self.assertEqual(getWordsWithSameEnd('existt',['exist','a','animals'],1),['exist'])
+        
     def test_getWordsWithCommonLetters(self):
         #test case for 3 common letters
         self.assertEqual(["Think", "They"], getWordsWithCommonLetters("Thane!", ["Do", "You", "Think", "Animals", "Exist", "Or", "Are", "They", "A", "Merry", "Affliction"], 3))
@@ -93,7 +84,11 @@ class test_spellChecker(unittest.TestCase):
         self.assertEqual([], getWordsWithCommonLetters("An", ["Do", "You", "Think", "Animals", "Exist", "Or", "Are", "They", "A", "Merry", "Affliction"], 0))
         #test case for asking for too many common letters
         self.assertEqual([], getWordsWithCommonLetters("An", ["Do", "You", "Think", "Animals", "Exist", "Or", "Are", "They", "A", "Merry", "Affliction"], 20))
-
+    def test_getWordsWithCommonPercent(self):
+        #Test for commonpercent at 70%
+        self.assertEqual(getWordsWithCommonPercent('Thane', ['Think','Than','Thought','Bye'], 70),['Than'])
+        #Test for commonpercebt at 50%
+        self.assertEqual(getWordsWithCommonPercent('Thane', ['Thank','Than','Thought','Bye'], 50),['Thank','Than'])
     def test_getSimilarityMetric(self):
         #general test case
         self.assertEqual(2, getSimilarityMetric("Apple", "Append"))
@@ -135,42 +130,48 @@ class test_spellChecker(unittest.TestCase):
 
     def test_getBestWords(self):
         #tests output of the first top word
-        self.assertEqual(["Do"], getBestWords((getSimilarityDict("Do", ["Do", "You", "Think", "Animals", "Exist", "Or", "Are", "They", "A", "Merry", "Affliction"])), 1))
+        self.assertEqual(["do"], getBestWords((getSimilarityDict("Do", ["do", "you", "think", "animals", "exist", "or", "sre", "they", "a", "merry", "affliction"])), 1))
         #test output of the two top words
-        self.assertEqual(["Do", "You"], getBestWords((getSimilarityDict("Do", ["Do", "You", "Think", "Animals", "Exist", "Or", "Are", "They", "A", "Merry", "Affliction"])), 2))  
+        self.assertEqual(["do", "you"], getBestWords((getSimilarityDict("Do", ["do", "you", "think", "animals", "exist", "er", "are", "they", "a", "merry", "affliction"])), 2))  
      
     def test_getWordSuggestionsV1(self):
         #tests a known result with topN = two values
-        self.assertEqual(["Animals", "Affliction"], getWordSuggestionsV1("Affable", "test_dictionary.txt", 5, 30, 2))
+        self.assertEqual(["merry", "are"], getWordSuggestionsV1("mmerry", "test_dictionary.txt", 5, 30, 2))
         #tests to constrain the result to a lesser top value
-        self.assertEqual(["Animals"], getWordSuggestionsV1("Affable", "test_dictionary.txt", 5, 30, 1))
+        self.assertEqual(["merry"], getWordSuggestionsV1("mmerry", "test_dictionary.txt", 5, 30, 1))
         #tests a range outside of the total # of words available
-        self.assertEqual(["Animals", "Affliction"], getWordSuggestionsV1("Affable", "test_dictionary.txt", 5, 30, 3))
+        self.assertEqual(["a","are"], getWordSuggestionsV1("a", "test_dictionary.txt", 5, 30, 3))
         #tests an extreme range
-        self.assertEqual(["Animals", "Affliction"], getWordSuggestionsV1("Affable", "test_dictionary.txt", 5, 30, 9))
+        self.assertEqual(["exist"], getWordSuggestionsV1("existt", "test_dictionary.txt", 5, 30, 9))
         #tests a 0 constraint
         self.assertEqual([], getWordSuggestionsV1("Affable", "test_dictionary.txt", 5, 30, 0))
-
+        #test case for multiple output
+        self.assertEqual(["Appelfrate", "Aperlprite"], getWordSuggestionsV1("Appleplate", "test_dictionary_2.txt",5, 30, 2))
+        self.assertEqual(["exist"], getWordSuggestionsV1("existt", "test_dictionary.txt", 2,75, 7))
+        #self.assertEqual(["they"], getWordSuggestionsV1("thy", "test_dictionary.txt", 2,75, 7))
     def test_getWordSuggestionsV2(self):
         #tests for known word that matches
-        self.assertEqual(["Animals"], getWordSuggestionsV2("Animals", "test_dictionary.txt", 2, 1))
+        #self.assertEqual(["animals"], getWordSuggestionsV2("animls", "test_dictionary.txt", 2, 1))
+        #tests for known word that matches
+        self.assertEqual(["exist"], getWordSuggestionsV2("existt", "test_dictionary.txt", 1, 2))
+        self.assertEqual(["they"], getWordSuggestionsV2("thy", "test_dictionary.txt", 1, 2))
         #test case for a 0 returned words
         self.assertEqual([], getWordSuggestionsV2("Animals", "test_dictionary.txt", 2, 0))
         #test case for more topN words than exist
-        self.assertEqual(["Animals"], getWordSuggestionsV2("Animals", "test_dictionary.txt", 2, 3))
+        self.assertEqual(["animals"], getWordSuggestionsV2("animals", "test_dictionary.txt", 2, 3))
         #similar to first test, single letter
-        self.assertEqual(["A"], getWordSuggestionsV2("A", "test_dictionary.txt", 1, 2))
+        self.assertEqual(["a"], getWordSuggestionsV2("a", "test_dictionary.txt", 1, 2))
         #test case for multiple output
         self.assertEqual(["Appelfrate", "Aperlprite"], getWordSuggestionsV2("Appleplate", "test_dictionary_2.txt", 2, 2))
         #test case for increased match, n requirement
         self.assertEqual(["Appelfrate"], getWordSuggestionsV2("Appleplate", "test_dictionary_2.txt", 3, 2))
 
     def test_getCombinedWordSuggestions(self):
-        #checks for expected outcome
-        self.assertEqual(["Appelfrate", "Aperlprite"], getCombinedWordSuggestions("Appleplate", "test_dictionary_2.txt"))
+        #checks for expected outcome#self.assertEqual(["Appelfrate"], getCombinedWordSuggestions("Appleplate", "test_dictionary_2.txt"))
         #checks for non-existant case
         self.assertEqual([], getCombinedWordSuggestions("Apple", "test_dictionary_2.txt"))
         #checks for alternate casev
-        self.assertEqual(["Animals"], getCombinedWordSuggestions("Animals", "test_dictionary_2.txt"))
-        
+        self.assertEqual(["Animals"], getCombinedWordSuggestions("Animls", "test_dictionary_2.txt"))
+        self.assertEqual(["exist"], getCombinedWordSuggestions("existt", "test_dictionary.txt"))
+        self.assertEqual(["they"], getCombinedWordSuggestions("thy", "test_dictionary.txt"))
 unittest.main()
